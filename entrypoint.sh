2edafc79860d0282125fd1cf6fc8ee6605eef338
Simon Scurrell <sjscurrell@gmail.com>
2023-05-08 11:50:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+while getopts p: flag
+do
+    case "${flag}" in
+        p) pathname=${OPTARG};;
+    esac
+done
+
 # Install Ruby
 chmod +x install_ruby.sh
 ./install-ruby.sh
@@ -12,8 +19,9 @@ sudo apt-get install -y coreutils gcc gcovr valgrind libc-dev
 sudo gem install --force ceedling:0.31.1
 
 # Navigate to unit tests
-echo 'Attempting to run unit tests at location: $1';
-cd $1
+echo 'Attempting to run unit tests at location:' $pathname;
+cd $pathname
+ls
 
 # Run unit tests
 ceedling test:all
