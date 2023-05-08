336318010e2fb3ee8bb13ed8454ac030738a8870
Simon Scurrell <sjscurrell@gmail.com>
2023-05-08 11:31:53 +0100

    Initial test release (#1)
    * Create action.yml
    
    * Create install-ruby.sh
    
    Script to install Ruby
    
    * Create entrypoint.sh
    
    * Update action.yml
    
    make a composite action
    
    * Update entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install Ruby
+chmod +x install_ruby.sh
+./install-ruby.sh
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y coreutils gcc gcovr valgrind libc-dev
+
+# Install ceedling
+gem install --force ceedling:0.31.1
+
+# Navigate to unit tests
+cd %1
+
+# Run unit tests
+ceedling test:all
